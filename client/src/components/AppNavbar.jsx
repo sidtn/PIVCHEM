09fa431c011afc7a2b89cc5f81294c0cbcda2359
Anatol Piven <sidtnlol@gmail.com>
2024-08-26 23:39:57 +0200

    login flow

--- client/src/components/AppNavbar.jsx
+++ client/src/components/AppNavbar.jsx
@@ -1,16 +1,60 @@
 import React from "react";
-import { Navbar, Container } from "react-bootstrap";
+import { Navbar, Container, NavDropdown } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 export const AppNavbar = () => {
+
+  let username = localStorage.getItem('username')
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    localStorage.removeItem('username');
+    localStorage.removeItem('user_id');
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('refresh_token');
+    navigate('/login');
+  };
+
+  const handleSettings = () => {
+    window.location.href = '/settings';
+  };
+
+  const textStyle = {
+    fontSize: '1.25rem',
+    color: 'black',
+    textDecoration: 'none'
+  };
+
   return (
     <Navbar className="bg-secondary">
       <Container>
-        <Navbar.Brand href="#home">PIVCHEM</Navbar.Brand>
+        <Link 
+          to="/editor" 
+          style={{
+            fontSize: '1.5rem',
+            color: '#063040',
+            textDecoration: 'none',
+            fontWeight: 'bold'
+          }}
+        >
+          PIVCHEM
+        </Link>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Signed in as: <a href="#login">Login</a>
-          </Navbar.Text>
+          {username? 
+            <NavDropdown title={<span style={textStyle}>{username}</span>} id="username-dropdown">
+              <NavDropdown.Item onClick={handleSettings}>Settings</NavDropdown.Item>
+            <NavDropdown.Divider />
+              <NavDropdown.Item onClick={handleLogout}>Log Out</NavDropdown.Item>
+            </NavDropdown>
+            :
+            <Link 
+              to="/login" 
+              style={textStyle}
+            >
+              Login
+            </Link>
+          }
         </Navbar.Collapse>
       </Container>
     </Navbar>
